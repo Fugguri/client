938204291b14588fb0bc53c0e5e89e142a5bdf80
Fugguri <fygguri@icloud.com>
2023-08-10 14:03:34 +0300

    first commit

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
 const socket = io('https://a29f0d485b3e.vps.myjino.ru')
-// const socket = io('http://99605bb2fc3e.vps.myjino.ru:49255')
+
 
 var mySocketId
 // register preliminary event listeners here:
