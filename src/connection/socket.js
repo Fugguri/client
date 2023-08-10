3418d76db7ce92fb9fc2824626988c2ac5e3ea54
Fugguri <fygguri@icloud.com>
2023-08-10 14:46:58 +0300

    first commit

--- src/connection/socket.js
+++ src/connection/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('https://a29f0d485b3e.vps.myjino.ru:80')
+const socket = io('https://a29f0d485b3e.vps.myjino.ru')
 // const socket = io('http://99605bb2fc3e.vps.myjino.ru:49255')
 
 var mySocketId
