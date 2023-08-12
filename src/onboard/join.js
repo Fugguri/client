b63b25636cf72bfa57c4e3b4d96c5129f3a47c3b
Fugguri <fygguri@icloud.com>
2023-08-12 13:53:38 +0300

    first commit

--- src/onboard/join.js
+++ src/onboard/join.js
@@ -27,7 +27,7 @@ class JoinTheGame extends React.Component {
             {
                 this.state.didGetUserName ?
                     <React.Fragment>
-                        {/* <JoinGame userName={this.props.userName} isCreator={false} /> */}
+                        <JoinGame userName={this.props.userName} isCreator={false} />
                         <ChessGame myUserName={this.props.userName} />
 
                     </React.Fragment>
