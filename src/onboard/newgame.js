4652d31f968467a0e181bfd52fcf2ab22998fe88
Fugguri <fygguri@icloud.com>
2023-08-12 14:48:33 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -33,38 +33,38 @@ class CreateNewGame extends React.Component {
     }
 
     render() {
+        return (
+            <React.Fragment>
+                {this.state.didGetUserName ?
+                    <Redirect to={"/game/" + gameid + "/" + username} >
+                        <button className="btn btn-success" style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px" }}>Start Game</button>
+                    </Redirect> :
+                    <div>
+                        <h1 style={{ textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px" }}>Your Username:</h1>
 
-        <React.Fragment>
-            {this.state.didGetUserName ?
-                <Redirect to={"/game/" + gameid + "/" + username} >
-                    <button className="btn btn-success" style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px" }}>Start Game</button>
-                </Redirect> :
-                <div>
-                    <h1 style={{ textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px" }}>Your Username:</h1>
+                        <input style={{ marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px" }}
+                            ref={this.textArea}
+                            onInput={this.typingUserName}></input>
 
-                    <input style={{ marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px" }}
-                        ref={this.textArea}
-                        onInput={this.typingUserName}></input>
-
-                    <button className="btn btn-primary"
-                        style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px" }}
-                        disabled={!(this.state.inputText.length > 0)}
-                        onClick={() => {
-                            // When the 'Submit' button gets pressed from the username screen,
-                            // We should send a request to the server to create a new room with
-                            // the uuid we generate here.
-                            this.props.didRedirect()
-                            this.props.setUserName(this.state.inputText)
-                            this.setState({
-                                didGetUserName: true
-                            })
-                            this.send()
-                        }}>Submit</button>
-                </div>
-            }
+                        <button className="btn btn-primary"
+                            style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px" }}
+                            disabled={!(this.state.inputText.length > 0)}
+                            onClick={() => {
+                                // When the 'Submit' button gets pressed from the username screen,
+                                // We should send a request to the server to create a new room with
+                                // the uuid we generate here.
+                                this.props.didRedirect()
+                                this.props.setUserName(this.state.inputText)
+                                this.setState({
+                                    didGetUserName: true
+                                })
+                                this.send()
+                            }}>Submit</button>
+                    </div>
+                }
 
 
-        </React.Fragment>
+            </React.Fragment>)
     }
 }
 
