ba805a5476c40d613a56ab9f6adab46d4b088f05
Fugguri <fygguri@icloud.com>
2023-08-13 07:24:59 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -66,33 +66,35 @@ class CreateNewGame extends React.Component {
         // !!! TODO: edit this later once you have bought your own domain. 
 
         return (<React.Fragment>
-            {this.state.didGetColor ?
+            {
+            
                 <Redirect to={"/new/" + this.state.username + "/" + this.state.gameId}>
                     <button className="btn btn-success" style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px" }}>Start Game</button>
                 </Redirect>
-                :
-                <div>
-                    <h1 style={{ textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px" }}>select chess color:</h1>
+            // this.state.didGetColor ?
+            //     :
+            //     <div>
+            //         <h1 style={{ textAlign: "center", marginTop: String((window.innerHeight / 3)) + "px" }}>select chess color:</h1>
 
-                    <input style={{ marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px" }}
-                        ref={this.textArea}
-                        onInput={this.typingColor}></input>
+            //         <input style={{ marginLeft: String((window.innerWidth / 2) - 120) + "px", width: "240px", marginTop: "62px" }}
+            //             ref={this.textArea}
+            //             onInput={this.typingColor}></input>
 
-                    <button className="btn btn-primary"
-                        style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px" }}
-                        disabled={!(this.state.color.length > 0)}
-                        onClick={() => {
+            //         <button className="btn btn-primary"
+            //             style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px", marginTop: "62px" }}
+            //             disabled={!(this.state.color.length > 0)}
+            //             onClick={() => {
 
-                            this.props.didRedirect()
-                            console.log(this.state)
-                            this.props.setUserName(this.state.username)
+            //                 this.props.didRedirect()
+            //                 console.log(this.state)
+            //                 this.props.setUserName(this.state.username)
 
-                            this.setState({
-                                didGetColor: true
-                            })
-                            this.send()
-                        }}>Submit</button>
-                </div>
+            //                 this.setState({
+            //                     didGetColor: true
+            //                 })
+            //                 this.send()
+            //             }}>Submit</button>
+            //     </div>
             }
         </React.Fragment>)
     }
