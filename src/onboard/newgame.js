4a41236adb5641404c60251cfa62d6577e31b75e
Fugguri <fygguri@icloud.com>
2023-08-14 01:08:39 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -84,14 +84,16 @@ class CreateNewGame extends React.Component {
                     join: true,
                     isAdmin: false
                 })
+                return ()
             }
         })
 
         return (<React.Fragment>
-            {this.state.join && !this.state.isAdmin ? <React.Fragment>
-                <Redirect to={"/game/" + this.state.gameId}><button className="btn btn-success" style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px" }}>Start Game</button></Redirect>
-
-            </React.Fragment> :
+            {this.state.join && !this.state.isAdmin ?
+                <React.Fragment>
+                    <JoinGame userName={this.props.userName} isCreator={false} />
+                    <ChessGame myUserName={this.props.userName} />
+                </React.Fragment> :
                 this.state.didGetColor ?
                     <Redirect to={"/new/" + this.state.gameId + "/" + this.state.username}>
                         <button className="btn btn-success" style={{ marginLeft: String((window.innerWidth / 2) - 60) + "px", width: "120px" }}>Start Game</button>
