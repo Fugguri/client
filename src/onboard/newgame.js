516c77eabc15181da76754862743621537880b85
Fugguri <fygguri@icloud.com>
2023-08-13 05:36:51 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -22,6 +22,7 @@ class CreateNewGame extends React.Component {
 
     constructor(props) {
         super(props);
+        this.setUserName = this.props.setUserName()
         this.textArea = React.createRef();
     }
 
