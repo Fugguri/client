748a5e571cbfed900860549725ab9242f65ac222
Fugguri <fygguri@icloud.com>
2023-08-13 07:23:01 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -84,6 +84,7 @@ class CreateNewGame extends React.Component {
                         onClick={() => {
 
                             this.props.didRedirect()
+                            console.log(this.state)
                             this.props.setUserName(this.state.username)
 
                             this.setState({
