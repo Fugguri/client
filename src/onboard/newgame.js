367fc2d833adb22d83bfb601337686c6c789ce3e
Fugguri <fygguri@icloud.com>
2023-08-13 22:27:55 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -31,18 +31,7 @@ class CreateNewGame extends React.Component {
         let a = socket.emit('isGameExist', this.props.gameId)
         console.log(a)
         console.log(rooms)
-        if (this.props.roomId)  {
 
-            this.setState({
-                isRoomExist: true,
-            })
-        }
-        if (this.props.room.creator === this.props.userName) {
-            this.setState({
-                creator: true
-            })
-
-        }
     }
 
     isCreator = () => {
