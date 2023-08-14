d0e94b1f2e3cd0295db43d5244bfe32a0a783640
Fugguri <fygguri@icloud.com>
2023-08-14 05:41:11 +0300

    first commit

--- src/onboard/newgame.js
+++ src/onboard/newgame.js
@@ -89,6 +89,7 @@ class CreateNewGame extends React.Component {
     join = () => {
         console.log("join")
         socket.emit('isGameExist', { gameId: this.props.gameId, userName: this.username })
+        this.props.didRedirect()
 
         return this.state.join && !this.state.isAdmin
 
