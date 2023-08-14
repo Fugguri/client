0ca7d73719fa624e80911fda46d54240be6bac8e
Fugguri <fygguri@icloud.com>
2023-08-14 05:12:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             <Onboard setUserName={setUserName} />
           </Route>
 
-          <Route path="/new/:gameid/:username" exact>
+          <Route path="/new/:gameid/:username(/:data)" exact>
             {didRedirect ?
               <React.Fragment>
                 <JoinGame userName={userName} isCreator={true} />
